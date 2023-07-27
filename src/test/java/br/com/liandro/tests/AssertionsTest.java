c356b4c4dc675981d41a6ac8421bb2d667b59354
Dernival Liandro <liandro.sys@gmail.com>
2023-07-26 23:39:26 -0300

    Hamcrest

--- src/test/java/br/com/liandro/tests/AssertionsTest.java
+++ src/test/java/br/com/liandro/tests/AssertionsTest.java
@@ -1,10 +1,15 @@
 package br.com.liandro.tests;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
@@ -47,6 +52,39 @@ public class AssertionsTest {
                 () -> assertThrows(NullPointerException.class, null),
                 () -> assertTrue(true, "This boolean condition did evaluate to true")
         );
+    }
+
+    @Test
+    void assertForMapTest() {
+        Map<String, Integer> theMap = new HashMap<>();
+        theMap.put("firstKey", 1);
+        theMap.put("secondKey", 2);
+        theMap.put("thirdKey", 3);
+
+        assertThat(theMap, hasKey("secondKey"));
+        assertThat(theMap, hasValue(3));
+    }
+
+    @Test
+    void assertForList() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, hasItem("thirdString"));
+    }
+
+    @Test
+    void assertForAnyOf() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, anyOf(hasItem("secondString"), hasItem("noString")));
+    }
+
+    @Test
+    void assertForContainsAnyOrder() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, containsInAnyOrder("thirdString", "firstString", "secondString"));
+        assertThat(theList, containsInRelativeOrder("firstString", "secondString", "thirdString"));
     }
 
 }
